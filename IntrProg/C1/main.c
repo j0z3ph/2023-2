bfd3fd981c6f2e21d7bd6b400ede4e5a7f1fe2d8
Jose Luis Cruz <j0z3ph@gmail.com>
2023-02-15 17:11:54 -0600

    Primer Programa

---
+++ IntrProg/C1/main.c
@@ -0,0 +1,18 @@
+/**
+ * @file main.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Mi primer programa.
+ * @version 0.1
+ * @date 2023-02-15
+ * 
+ * @copyright GPL v3
+ * 
+ */
+
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("HOLO\n");
+    return 0;
+}
