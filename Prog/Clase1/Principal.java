95a16447c7da4fdc8a724f9f64c18c4976bde958
Jose Luis Cruz <j0z3ph@gmail.com>
2023-02-17 14:09:23 -0600

    Clase 1

---
+++ Prog/Clase1/Principal.java
@@ -0,0 +1,28 @@
+/**
+ * Principal.java
+ * Mi primer programa en Java
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @date 2023-02-17
+ * GPL v3
+ */
+
+public class Principal {
+
+    /**
+     * Funcion principal.
+     * @param args Parametros de entrada del programa
+     */
+    public static void main(String[] args) {
+        System.out.println(getMessage());
+    }
+
+
+    /**
+     * Devuelve un mensaje de Bienvenida en lenguaje
+     * inclusivo.
+     * @return El mensaje
+     */
+    public static String getMessage() {
+        return "Hole e Todes";
+    }
+}
