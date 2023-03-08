060d247992aee4b6c60f3bdebf0f742adf53550d
Jose Luis Cruz <j0z3ph@gmail.com>
2023-03-08 13:20:14 -0600

    Prueba

--- Prog/Clase7/Principal.java
+++ Prog/Clase7/Principal.java
@@ -9,7 +9,7 @@
 public class Principal {
     public static void main(String[] args) {
         Cuadrado cuadrado = new Cuadrado(4);
-        //Figura fig = new Figura(3, 4);
+        //Figura fig = new Figura(3, 4); 
 
         System.out.println("Area: " + cuadrado.area() + 
         "\nPerimetro: " + cuadrado.perimetro());
