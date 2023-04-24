2469a9ad93ee24c6e28c5201438d61b06e80fb66
Jose Luis Cruz <j0z3ph@gmail.com>
2023-04-24 17:20:18 -0600

    Ejemplos

---
+++ IntrProg/C19/main.c
@@ -0,0 +1,26 @@
+/**
+ * @file main.c
+ * @author Jose Luis
+ * @brief 
+ * @version 0.1
+ * @date 2023-04-24
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void magia() {
+    int *ptr = (int*)malloc(sizeof(int) * 100);
+}
+
+
+int main()
+{
+    while(1)
+        magia();
+    return 0;
+}
